bdd0123d475294f3edf12ebbe4733afa4a7eea8e
Paolo Payumo <paolo.payumo@yahoo.com>
2017-08-31 20:32:55 -0700

    Finish Ex_1_1_09

---
+++ src/algorithms/1-Fundamentals/1-1-BasicProgramingModel/Ex_1_1_09.java
@@ -0,0 +1,14 @@
+public class Ex_1_1_09 {
+  public static void main(String[] args) {
+    int N = 322;
+    System.out.println(Integer.toBinaryString(N));
+    
+    String s = "";
+    for (int n = N; n > 0; n /= 2) {
+      s = (n % 2) + s;      
+    }
+    
+    System.out.println(s);
+  }
+}
+
