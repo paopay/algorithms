3f4e45c601bccf6d7f956eac61629952624bbd86
Paolo Payumo <paolo.payumo@yahoo.com>
2017-08-31 20:10:03 -0700

    Complete Ex_1_1_05

--- src/algorithms/1-Fundamentals/1-1-BasicProgramingModel/Ex_1_1_05.java
+++ src/algorithms/1-Fundamentals/1-1-BasicProgramingModel/Ex_1_1_05.java
@@ -1,18 +1,16 @@
-
-
 //Write a code fragment that prints true if the double
 //variables x and y are both strictly between 0 
 //and 1 and false otherwise.
 
 public class Ex_1_1_05 {
   public static void main(String[] args) {
-    int f = 0;
-    int g = 1;
-    for (int i = 0; i <= 15; i++)
-    {
-      System.out.println(f);
-      f = f + g;
-      g = f - g;
+    double x = 0.3;
+    double y = 0.2;
+
+    if (x > 0 && x < 1 && y > 0 && y < 1) {
+      System.out.println(true);  
+    } else {
+      System.out.println(false);
     }
   }
 }
