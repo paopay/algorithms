e61cfdfbd9138bc3edfd480f71b097738f08bf10
Paolo Payumo <paolo.payumo@yahoo.com>
2017-09-10 18:39:35 -0700

    Add FixedCapacityStackOfStrings isFull method

---
+++ src/1-Fundamentals/1-3-BagsQueuesStacks/FixedCapacityStackOfStrings.java
@@ -0,0 +1,38 @@
+//1.3.1 Add a method isFull() to FixedCapacityStackOfStrings.
+
+public class FixedCapacityStackOfStrings {
+  private String[] a; // stack entries
+  private int N; // size
+
+  public FixedCapacityStackOfStrings(int cap) {
+    a = new String[cap];
+  }
+
+  public boolean isFull() {
+    return N == (cap-1);
+  }
+  
+  public boolean isEmpty() {
+    return N == 0;
+  }
+
+  public int size() {
+    return N; 
+  }
+
+  public void push(String item) {
+    a[N++] = item;
+  }
+
+  public String pop() {
+    return a[--N]; 
+  }
+  
+  public static void main(String[] args) {
+    FixedCapacityStackOfStrings s;
+    s = new FixedCapacityStackOfStrings(1);
+    
+    s.push("one");
+    System.out.println(s.isFull());
+  }
+}
