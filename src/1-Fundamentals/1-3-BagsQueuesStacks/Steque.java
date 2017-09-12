5989f313b4b5c4c4637a2ab520948bf1b053cba0
Paolo Payumo <paolo.payumo@yahoo.com>
2017-09-11 22:39:04 -0700

    Add Steque pop, isempty, enqueue

---
+++ src/1-Fundamentals/1-3-BagsQueuesStacks/Steque.java
@@ -0,0 +1,60 @@
+//1.3.32 Steque. A stack-ended queue or steque is a data type 
+//that supports push, pop, and enqueue. Articulate an API 
+//for this ADT. Develop a linked-list-based implementation.
+
+public class Steque<Item> {
+  private Node first, last;
+  
+  private class Node {
+    Item item;
+    Node next;
+  }
+  
+  public Steque() {
+    first = last = null;
+  }
+  
+  public void enqueue(Item item) {
+    Node oldLast = last;
+    last = new Node();
+    last.item = item;
+    last.next = null;
+    
+    if (isEmpty()) {
+      first = last;
+    } else {
+      oldLast.next = last; 
+    }
+  }
+  
+  public void push(Item item) {
+    
+  }
+  
+  public boolean isEmpty() {
+    return first == null || last == null;
+  }
+  
+  public Item pop() {
+    Item item = first.item;
+    first = first.next;
+    return item;
+  }
+  
+  public static void main(String[] args) {
+    Steque<Integer> steque = new Steque<Integer>();
+    steque.enqueue(1);
+    System.out.println(steque.first.item);
+    System.out.println(steque.isEmpty());
+    
+    steque.pop();
+    System.out.println(steque.isEmpty());
+    
+    steque.enqueue(2);
+    steque.enqueue(3);
+    System.out.println(steque.first.next.item);
+    
+    
+   
+  }
+}
