d90c4547565243b07dc4683ea23d15dd4cc76b3c
Paolo Payumo <paolo.payumo@yahoo.com>
2017-09-24 22:51:08 -0700

    Finish hot or cold

--- src/1-Fundamentals/1-4-AnalysisOfAlgorithms/HotOrCold.java
+++ src/1-Fundamentals/1-4-AnalysisOfAlgorithms/HotOrCold.java
@@ -7,48 +7,54 @@
 //guesses. Then design an algorithm that finds the secret number in
 //at most ~ 1 lg N guesses.
 
-import java.util.Scanner;
 import java.util.Random;
 
 public class HotOrCold {
   
   public static void main(String[] args) {
     Random rand = new Random();
-    Scanner keyboard = new Scanner(System.in);
     int N = Integer.parseInt(args[0]);
     int number = rand.nextInt(N);
+    int lo = 1;
+    int hi = N;
+    
     int lastGuess = 0;
-    int currentGuess = 0;
+    int currentGuess = 1;
     int count = 0;
     int lastGuessDiff = 0;
     int currentGuessDiff = 0;
+    
     System.out.println(number);
+    
     while (lastGuess != number) {
-      System.out.println("enter an integer");
-      currentGuess = keyboard.nextInt();
-      
-      if (count == 0) {
-        count++;
-        lastGuess = currentGuess;
-        continue;
-      }
+      System.out.println("-----------------");
+      System.out.println("Current guess is " + currentGuess);
       
       lastGuessDiff = Math.abs(lastGuess - number);
       currentGuessDiff = Math.abs(currentGuess - number);
+      
+      lastGuess = currentGuess;
       
       if (currentGuessDiff > lastGuessDiff) {
         System.out.println("Colder");
       } else if (currentGuessDiff < lastGuessDiff) {
         System.out.println("Hotter");
       } else {
-        System.out.println("Neither Hotter nor Colder");
+        System.out.println("Neither Hotter or Colder");
       }
       
-      lastGuess = currentGuess;
+      if (currentGuess > number) {
+        hi = currentGuess;
+        currentGuess = (currentGuess + lo) / 2;
+      } else if (currentGuess < number) {
+        lo = currentGuess;
+        currentGuess = (currentGuess + hi) / 2;
+      }
+      
       count++;
     }
     
-    System.out.println("You got it!");
+    System.out.println("You got it in " + count + " guesses!");
   }
 
 }
